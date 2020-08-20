9d33124954f79e9f19f50985c575e22787f89677
Gabriel Sroka <gabrielsroka@gmail.com>
2020-08-19 19:59:04 -0700

    Update and rename GetOktaUser.py to get_okta_user.py

--- get_okta_user.py
+++ get_okta_user.py
@@ -4,11 +4,11 @@ url = '...'
 token = '...'
 
 headers = {
-    'Authorization': 'SSWS ' + token,
+    'Authorization': f'SSWS {token}',
     'Accept': 'application/json'
 }
 
-res = requests.get(url + '/api/v1/users/me', headers=headers)
+res = requests.get(f'{url}/api/v1/users/me', headers=headers)
 if res.ok:
     user = res.json()
     print(user)
