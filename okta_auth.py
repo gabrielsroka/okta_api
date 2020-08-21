b787cf936a573e35759a02abb9cdc732c100cc09
Gabriel Sroka <gabrielsroka@gmail.com>
2020-08-21 11:16:43 -0700

    Update okta_auth.py

--- okta_auth.py
+++ okta_auth.py
@@ -1,5 +1,5 @@
 """Call Okta APIs using Session (cookies) and xsrfToken -- just like a browser. SSWS API Token is not needed.
-​
+
 ### CAUTION: Code to call private APIs is not supported by Okta. The APIs can change at any time.
 ###          Test in a dev or oktapreview tenant before trying this in production.
 
@@ -21,7 +21,7 @@ session = requests.Session()
 
 def main():
     sign_in()
-    user = get_user()
+    user = get_user('me')
     admin_xsrf_token = admin_sign_in()
     send_notification(admin_xsrf_token, user['id'])
     get_user_factors(user['id'])
@@ -32,7 +32,7 @@ def sign_in():
     password = getpass.getpass()
 
     print('Signing in...')
-    response = session.post(okta_url + '/api/v1/authn', json={'username': username, 'password': password})
+    response = session.post(f'{okta_url}/api/v1/authn', json={'username': username, 'password': password})
     authn = response.json()
     if not response.ok:
         print(authn['errorSummary'])
@@ -43,12 +43,12 @@ def sign_in():
     else:
         token = authn['sessionToken']
 
-    session.get(okta_url + '/login/sessionCookieRedirect?redirectUrl=/&token=' + token)
+    session.get(f'{okta_url}/login/sessionCookieRedirect?redirectUrl=/&token={token}' )
 
 def send_push(factors, state_token):
     print('Push MFA...')
     push_factors = [f for f in factors if f['factorType'] == 'push']
-    if len(push_factors) == 0:
+    if not push_factors:
         print('Push factor not found')
         exit()
     push_url = push_factors[0]['_links']['verify']['href']
@@ -65,14 +65,14 @@ def send_push(factors, state_token):
             exit()
 
 def admin_sign_in():
-    response = session.get(okta_url + '/home/admin-entry')
+    response = session.get(f'{okta_url}/home/admin-entry')
     match = re.search(r'"token":\["(.*)"\]', response.text)
     if not match:
         print('admin_sign_in: token not found. Go to Security > General and disable Multifactor for Administrators.')
         exit()
     body = {'token': match.group(1)}
     
-    response = session.post(okta_admin_url + '/admin/sso/request', data=body)
+    response = session.post(f'{okta_admin_url}/admin/sso/request', data=body)
     match = re.search(r'<span.* id="_xsrfToken">(.*)</span>', response.text)
     admin_xsrf_token = match.group(1)
     return admin_xsrf_token
@@ -85,16 +85,16 @@ def send_notification(admin_xsrf_token, userid):
         'groups': []
     }
     headers = {'X-Okta-XsrfToken': admin_xsrf_token}
-    notification = session.post(okta_admin_url + '/api/internal/admin/notification', json=body, headers=headers).json()
-    print(notification)
+    notification = session.post(f'{okta_admin_url}/api/internal/admin/notification', json=body, headers=headers).json()
+    print('\nNotification:\n', notification)
 
-def get_user():
-    user = session.get(okta_url + '/api/v1/users/me').json()
-    print(user)
+def get_user(userid):
+    user = session.get(f'{okta_url}/api/v1/users/{userid}').json()
+    print('\nUser:\n', user)
     return user
 
 def get_user_factors(userid):
-    factors = session.get(okta_url + '/api/v1/users/' + userid +  '/factors').json()
-    print(factors)
+    factors = session.get(f'{okta_url}/api/v1/users/{userid}/factors').json()
+    print('\nFactors:\n', factors)
 
 main()
