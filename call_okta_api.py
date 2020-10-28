346d30b41c5a9c329eee901b3a2b4e4dfd5e14ac
Gabriel Sroka <gabrielsroka@gmail.com>
2020-10-28 15:21:27 -0700

    Update call_okta_api.py

--- call_okta_api.py
+++ call_okta_api.py
@@ -1,10 +1,29 @@
 import okta_api
+import time
+
+def test_push():
+    user_id = okta_api.get_user('me').json()['id']
+    factors = okta_api.get_user_factors(user_id).json()
+    push_factors = [f for f in factors if f['factorType'] == 'push']
+    if not push_factors:
+        print('Push factor not found')
+        return
+    resp = okta_api.issue_user_factor_challenge(user_id, push_factors[0]['id']).json()
+    print('Sent push')
+    while True:
+        result = resp['factorResult']
+        print(result)
+        if result == 'WAITING':
+            time.sleep(4) # 4 seconds
+            resp = okta_api.session.get(resp['_links']['poll']['href']).json()
+        elif result in ['SUCCESS', 'REJECTED', 'TIMEOUT']:
+            return
 
 def export_users():
     # Export to CSV.
     print('Getting users.')
     users = []
-    for page in okta_api.get_user_pages(filter='profile.lastName eq "Doe"'): #, limit=2):
+    for page in okta_api.get_user_pages(filter='profile.lastName eq "Doe"'): # limit=2
         for user in page.json():
             users.append({
                 'id': user['id'], 
@@ -20,9 +39,28 @@ def get_schemas():
     for page in okta_api.get_app_pages():
         for app in page.json():
             r = okta_api.get_app_schema(app['id'])
-            if r.ok: # Skip bookmark apps.
+            if r.ok: # Skip bookmark apps, etc.
                 schema = r.json()
                 print(app['id'], app['label'], schema['definitions']['custom'])
+
+def get_app_groups(app):
+    LIMIT_REMAINING = 10
+    for page in okta_api.get_app_group_pages(app['id'], expand='group'):
+        groups = page.json()
+        for group in groups:
+            app_groups.append({'app': app['label'], 'group': group['_embedded']['group']['profile']['name']})
+        if not groups:
+            app_groups.append({'app': app['label'], 'group': '(no groups)'})
+        okta_api.snooze(page, LIMIT_REMAINING)
+
+app_groups = []
+def get_apps_and_groups():
+    for page in okta_api.get_app_pages():
+        for app in page.json():
+            print('fetching', app['label'])
+            get_app_groups(app)
+    okta_api.export_csv('app_groups.csv', app_groups, app_groups[0].keys())
+
 
 
 # def main():
