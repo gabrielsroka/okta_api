74a6fc9b8e6e81cd299b36d8f18afd7c44eb6185
Gabriel Sroka <gabrielsroka@gmail.com>
2022-01-21 13:53:08 -0800

    Create queue_tee.py

---
+++ queue_tee.py
@@ -0,0 +1,36 @@
+import okta.client
+import asyncio
+
+limit = 75 # see https://developer.okta.com/docs/reference/rl-additional-limits/#concurrent-rate-limits
+
+users = []
+counts = [0] * limit
+client = okta.client.Client()
+
+async def main():
+    print('get users')
+    global users
+    users, resp, _ = await client.list_users()
+    for i in range(2):
+        more_users, _ = await resp.next() if resp.has_next() else (None, None)
+        users.extend(more_users)
+    users = users[0:525]
+
+    print('start tasks')
+    tasks = [task(i) for i in range(limit)]
+    await asyncio.gather(*tasks)
+
+    counts.sort()
+    for i in counts:
+        print(i)
+    print(sum(counts))
+
+async def task(i):
+    while users:
+        user = users.pop()
+        gs, r, _ = await client.list_user_groups(user.id)
+        counts[i] += 1
+        # print(i, user.id) # , ', '.join([g.id for g in gs]))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
