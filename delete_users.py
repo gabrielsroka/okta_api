53282ba0f35bb7d6f4c6c2d763ec730bd57e860d
Gabriel Sroka <gabrielsroka@gmail.com>
2022-03-23 08:48:48 -0700

    Update delete_users.py

--- delete_users.py
+++ delete_users.py
@@ -2,22 +2,17 @@ from okta.client import Client as OktaClient
 import asyncio
 
 async def main():
-    client = OktaClient()
-    query_params = {'filter': 'status eq "SUSPENDED"'} # SUSPENDED, DEPROVISIONED, etc. see https://developer.okta.com/docs/reference/api/users/#user-properties
-    users, resp, err = await client.list_users(query_params)
-    while users:
-        for user in users:
-            print(user.profile.login, user.status) # Add more properties here.
-            if user.status != 'DEPROVISIONED': # Must call deactivate_or_delete_user twice.
+    async with OktaClient() as client:
+        query_params = {'filter': 'status eq "SUSPENDED"'} # SUSPENDED, DEPROVISIONED, etc. see https://developer.okta.com/docs/reference/api/users/#user-properties
+        users, resp, err = await client.list_users(query_params)
+        while users:
+            for user in users:
+                print(user.profile.login, user.status) # Add more properties here.
+                if user.status != 'DEPROVISIONED': # Must call deactivate_or_delete_user twice.
+                    await client.deactivate_or_delete_user(user.id)
                 await client.deactivate_or_delete_user(user.id)
-            await client.deactivate_or_delete_user(user.id)
 
-        users = await next(resp)
-
-async def next(resp):
-    if resp.has_next():
-        page, err = await resp.next()
-        return page
+            users, err = await resp.next() if resp.has_next() else (None, None)
 
 
 loop = asyncio.get_event_loop()
