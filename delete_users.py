58548e060d243ca33b8236cadee2d346b8cd5ca8
Gabriel Sroka <gabrielsroka@gmail.com>
2022-11-18 10:09:24 -0800

    Update delete_users.py

--- delete_users.py
+++ delete_users.py
@@ -7,8 +7,8 @@ async def main():
     global client
     async with okta.client.Client() as client:
         # SUSPENDED, DEPROVISIONED, etc. see https://developer.okta.com/docs/reference/api/users/#user-properties
-        async for user in get_users(filter='status eq "SUSPENDED"'):
-            print(user.profile.login, user.status) # Add more properties here.
+        async for user in get_users(search='status eq "SUSPENDED"'):
+            print(user.id, user.profile.login, user.status) # Add more properties here.
             if user.status != 'DEPROVISIONED': # Must call deactivate_or_delete_user twice.
                 await client.deactivate_or_delete_user(user.id)
             await client.deactivate_or_delete_user(user.id)
